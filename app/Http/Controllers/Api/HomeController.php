998f86d8214672fbe040d9df130f1d00ec0ffc9d
wael kasouha <waelkas99@gmail.com>
2023-10-27 08:55:36 -0400

    Home Controller

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -5,11 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\ForSaleCard;
 use App\Models\Landing;
-use App\Models\Method;
-use App\Models\Payment;
-use App\Models\Upload;
-use Illuminate\Http\Request;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class HomeController extends Controller
 {
@@ -37,54 +32,6 @@ class HomeController extends Controller
             'cardsForSale' => $cardsForSale,
         ];
  
-        return response()->json($data);
-    }
-
-    public function isAdmin()
-    {
-        $data = [
-            'message' => 'You are an admin',
-        ];
-
-        return response()->json($data);
-    }
-
-    public function isUser()
-    {
-        $countPenddingUploadedCards = Upload::where('user_id', auth()->user()->id)->count();
-        $methods = Method::where('status', 1)->get();
-        
-        $data = [
-            'countPenddingUploadedCards' => $countPenddingUploadedCards,
-            'methods' => $methods,
-        ];
-
-        return response()->json($data);
-    }
-
-    public function buyCard($id)
-    {
-        $card = ForSaleCard::findOrFail($id);
-        $discounts =  $card->discounts;
-
-        $data = [
-            'card' => $card,
-            'discounts' => $discounts,
-        ];
-
-        return response()->json($data);
-    }
-
-    public function getTransectionNumber(Request $request)
-    {
-        $tr = Payment::findOrFail($request->country_id);
-
-        $data = [
-            'transaction_number' => $tr->transaction_number,
-            'amount' => $tr->amount,
-            // Include any other relevant data from the Payment model
-        ];
-
         return response()->json($data);
     }
 }
